49a18c2c59ec0d37f589248f057a16ac19516650
Nicolás Berríos Verboven <nbverboven@gmail.com>
2021-04-30 10:43:25 -0300

    Clarifico un poco la matriz de rotación que se devuelve

--- tp2/ejercicio.js
+++ tp2/ejercicio.js
@@ -1,4 +1,4 @@
-// Esta función construye una matriz de transfromación de 3x3 en coordenadas homogéneas 
+// Esta función construye una matriz de transformación de 3x3 en coordenadas homogéneas 
 // utilizando los parámetros de posición, rotación y escala. La estructura de datos a 
 // devolver es un arreglo 1D con 9 valores en orden "column-major". Es decir, para un 
 // arreglo A[] de 0 a 8, cada posición corresponderá a la siguiente matriz:
@@ -9,7 +9,7 @@
 // 
 // Se deberá aplicar primero la escala, luego la rotación y finalmente la traslación. 
 // Las rotaciones vienen expresadas en grados. 
-function BuildTransform( positionX, positionY, rotation, scale )
+const BuildTransform = ( positionX, positionY, rotation, scale ) =>
 {
 	const mt = translationM(positionX, positionY);
 	const mr = rotationM(rotation);
@@ -25,15 +25,19 @@ const translationM = (x, y) => Array(1, 0, 0, 0, 1, 0, x, y, 1);
 const rotationM = degrees => {
 	const rad = Math.PI * Math.abs(degrees) / 180;
 	const cos = Math.cos(rad);
-	const sin = Math.sin(rad) * (degrees < 0 ? -1 : 1);
+	const sin = Math.sin(rad);
 
-	return Array(cos, sin, 0, -sin, cos, 0, 0, 0, 1);
+	if (degrees < 0) {
+		return Array(cos, -sin, 0, sin, cos, 0, 0, 0, 1);
+	} else {
+		return Array(cos, sin, 0, -sin, cos, 0, 0, 0, 1);
+	}
 }
 
-// Esta función retorna una matriz que resula de la composición de trasn1 y trans2. Ambas 
+// Esta función retorna una matriz que resulta de la composición de trans1 y trans2. Ambas 
 // matrices vienen como un arreglo 1D expresado en orden "column-major", y se deberá 
 // retornar también una matriz en orden "column-major". La composición debe aplicar 
-// primero trans1 y luego trans2. 
+// primero trans1 y luego trans2.
 function ComposeTransforms( trans1, trans2 )
 {
 	return Array(1,0,0,0,1,0,0,0,1);
