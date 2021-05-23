05bf15f6f1170417debeb6b8b362e03a5f1dd8d6
Nicolás Berríos Verboven <nbverboven@gmail.com>
2021-05-23 14:30:33 -0300

    Dibujo una recta entre el primer y el último punto de control

--- tp3/ejercicio.js
+++ tp3/ejercicio.js
@@ -1,71 +1,102 @@
-// Completar la implementación de esta clase y el correspondiente vertex shader. 
+// Completar la implementación de esta clase y el correspondiente vertex shader.
 // No será necesario modificar el fragment shader a menos que, por ejemplo, quieran modificar el color de la curva.
-class CurveDrawer 
+class CurveDrawer
 {
-  // Inicialización de los shaders y buffers
-  constructor()
-  {
-    // Creamos el programa webgl con los shaders para los segmentos de recta
-    this.prog   = InitShaderProgram( curvesVS, curvesFS );
+	// Inicialización de los shaders y buffers
+	constructor()
+	{
+		// Creamos el programa webgl con los shaders para los segmentos de recta
+		this.prog = InitShaderProgram(curvesVS, curvesFS);
 
-    // [Completar] Incialización y obtención de las ubicaciones de los atributos y variables uniformes
-        
-    // Muestreo del parámetro t
-    this.steps = 100;
-    var tv = [];
-    for ( var i=0; i<this.steps; ++i ) {
-      tv.push( i / (this.steps-1) );
+		// [Completar] Incialización y obtención de las ubicaciones de los atributos y variables uniformes
+    this.p0 = gl.getUniformLocation(this.prog, 'p0');
+    this.p3 = gl.getUniformLocation(this.prog, 'p3');
+    this.mvp = gl.getUniformLocation(this.prog, 'mvp');
+
+    this.t = gl.getAttribLocation(this.prog, 't');
+
+		// Muestreo del parámetro t
+		this.steps = 100;
+		const tv = [];
+		for (var i = 0; i < this.steps; ++i) {
+		  tv.push(i / (this.steps-1));
+		}
+
+		// [Completar] Creacion del vertex buffer y seteo de contenido
+    this.buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0.0, 1.0]), gl.STATIC_DRAW);
+	}
+
+	// Actualización del viewport (se llama al inicializar la web o al cambiar el tamaño de la pantalla)
+	setViewport( width, height )
+	{
+		// [Completar] Matriz de transformación.
+    var trans = [ 2/width,0,0,0,  0,-2/height,0,0, 0,0,1,0, -1,1,0,1 ];
+
+		// [Completar] Binding del programa y seteo de la variable uniforme para la matriz.
+    gl.useProgram(this.prog);
+    gl.uniformMatrix4fv(this.mvp, false, trans);
+	}
+
+	updatePoints( pt )
+	{
+		// [Completar] Actualización de las variables uniformes para los puntos de control
+		// [Completar] No se olviden de hacer el binding del programa antes de setear las variables
+		// [Completar] Pueden acceder a las coordenadas de los puntos de control consultando el arreglo pt[]:
+		// var x = pt[i].getAttribute("cx");
+		// var y = pt[i].getAttribute("cy");
+		gl.useProgram(this.prog);
+
+    const updateControlPoint = (controlPoint, newValue) => {
+      const coordinates = [newValue.getAttribute("cx"), newValue.getAttribute("cy")]; 
+      return gl.uniform2fv(controlPoint, new Float32Array(coordinates));
     }
-    
-    // [Completar] Creacion del vertex buffer y seteo de contenido
-  }
 
-  // Actualización del viewport (se llama al inicializar la web o al cambiar el tamaño de la pantalla)
-  setViewport( width, height )
-  {
-    // [Completar] Matriz de transformación.
-    // [Completar] Binding del programa y seteo de la variable uniforme para la matriz. 
-  }
+    updateControlPoint(this.p0, pt[0]);
+    updateControlPoint(this.p3, pt[3]);
+	}
 
-  updatePoints( pt )
-  {
-    // [Completar] Actualización de las variables uniformes para los puntos de control
-    // [Completar] No se olviden de hacer el binding del programa antes de setear las variables 
-    // [Completar] Pueden acceder a las coordenadas de los puntos de control consultando el arreglo pt[]:
-    // var x = pt[i].getAttribute("cx");
-    // var y = pt[i].getAttribute("cy");
-  }
+	draw()
+	{
+		// [Completar] Dibujamos la curva como una LINE_STRIP
+		// [Completar] No se olviden de hacer el binding del programa y de habilitar los atributos de los vértices
+		gl.useProgram(this.prog);
 
-  draw()
-  {
-    // [Completar] Dibujamos la curva como una LINE_STRIP
-    // [Completar] No se olviden de hacer el binding del programa y de habilitar los atributos de los vértices
-  }
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+
+		gl.vertexAttribPointer(this.t, 1, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(this.t);
+
+		gl.drawArrays(gl.LINE_STRIP, 0, 2);
+	}
 }
 
 // Vertex Shader
-//[Completar] El vertex shader se ejecuta una vez por cada punto en mi curva (parámetro step). No confundir punto con punto de control.
+// [Completar] El vertex shader se ejecuta una vez por cada punto en mi curva (parámetro step). No confundir punto con punto de control.
 // Deberán completar con la definición de una Bezier Cúbica para un punto t. Algunas consideraciones generales respecto a GLSL: si
 // declarás las variables pero no las usás, no se les asigna espacio. Siempre poner ; al finalizar las sentencias. Las constantes
 // en punto flotante necesitan ser expresadas como X.Y, incluso si son enteros: ejemplo, para 4 escribimos 4.0
 var curvesVS = `
-  attribute float t;
-  uniform mat4 mvp;
-  uniform vec2 p0;
+	attribute float t;
+	uniform mat4 mvp;
+	uniform vec2 p0;
   uniform vec2 p1;
   uniform vec2 p2;
-  uniform vec2 p3;
-  void main()
-  { 
-    gl_Position = vec4(0,0,0,1);
-  }
+	uniform vec2 p3;
+
+	void main()
+	{
+    vec2 pos = (1.0 - t) * p0 + t * p3;
+		gl_Position = mvp * vec4(pos,0,1);
+	}
 `;
 
 // Fragment Shader
 var curvesFS = `
-  precision mediump float;
-  void main()
-  {
-    gl_FragColor = vec4(0,0,1,1);
-  }
+	precision mediump float;
+	void main()
+	{
+		gl_FragColor = vec4(0,0,1,1);
+	}
 `;
